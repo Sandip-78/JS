787e772bf3a3abe70d649c6e94a79765b67954c8
Sandip Parmar <sandipparmar7813@gmail.com>
2026-04-26 01:33:46 +0530

    hosting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,3 +10,46 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// hosting happens in js , it means we can call the function before declaring it but it will work only for function declaration not for function expression
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+// temporal dead zone happend when we try to access the variable before declaring it and it will give us a reference error
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
